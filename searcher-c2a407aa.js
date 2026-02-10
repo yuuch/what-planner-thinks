d2a40a67f2982ba43b40b1e0896fb442fd885a7a
yuuch <yuuch@users.noreply.github.com>
2026-02-10 08:48:13 +0000

    deploy: c0f8a03314a86df670ed7580351c36b2ca3119f3

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c0d10b8c.js',
+                path_to_root + 'searchindex-8651b419.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
